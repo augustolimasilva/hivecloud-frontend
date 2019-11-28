a4307f633774f512b0a898b683fc860403fd7e1d
Augusto Silva <augusto_ls17@hotmail.com>
2019-11-28 12:05:01 -0300

    Ajustes de vunerabilidades e ajuste nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent} from '../crud/cadastro/cadastro.component';
 
 const routes: Routes = [
-  {path : 'home', component : CadastroComponent}
+  {path : 'cadastro', component : CadastroComponent},
 ];
 
 @NgModule({
   exports: [ RouterModule ],
-  imports: [ RouterModule.forRoot(routes)]
+  imports: [ RouterModule.forRoot(routes)],
 })
 export class AppRoutingModule {}
