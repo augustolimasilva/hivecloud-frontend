3d18e8858fa66a1cadd73bc2ae2db6d95662cb4f
Augusto Silva <augusto_ls17@hotmail.com>
2019-11-29 12:46:58 -0300

    Criação do model de transportadora

---
+++ src/model/transportadora.ts
@@ -0,0 +1,17 @@
+export class Transportadora{
+    email: string;
+    nome: string;
+    empresa: string;
+    telefone: string;
+    celular: string;
+    whatsapp: string;
+    modalidade: string;
+    cep: string;
+    rua: string;
+    numero: string;
+    bairro: string;
+    cidade: string;
+    uf: string;
+    logo: Blob;
+    snAceitouTermos: string;
+}
